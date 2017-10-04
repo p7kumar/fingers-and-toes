730e038de766dc581363ce5da29a9e75dcbc0cdc
pranaykumar <pranaycd7@gmail.com>
2017-10-03 20:02:37 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,99 @@
+var fingers = document.getElementById('fingers-box');
+var toes = document.getElementById('toes-box');
+
+function startTimer(){
+    var inputNumber = document.getElementById('input-number').value;
+    initializeTimer(inputNumber);
+}
+
+function restartTimer(){
+    var inputNumber = document.getElementById('input-number').value;
+    document.getElementById('display-number').innerHTML = "0";
+    initializeTimer(inputNumber);
+}
+
+function resetTimer(){
+    clearAllIntervals();
+    document.getElementById('input-number').value = "";
+    document.getElementById('display-number').innerHTML = "0";
+    fingers.style.color = 'black';
+    fingers.style.backgroundColor = 'transparent';
+    toes.style.color = 'black';
+    toes.style.backgroundColor = 'transparent';
+}
+
+function clearAllIntervals(){
+    for(var i=1;i<999;i++){
+        clearInterval(i);
+    }
+}
+
+function initializeTimer(maxNumber){
+    clearAllIntervals();
+    var startNumber = 1;
+    var displayNumber = document.getElementById('display-number');
+    var intervalId = setInterval(function(){
+        var newNumber = startNumber++;
+        if(newNumber < maxNumber){
+            if(newNumber % 5 === 0 && newNumber % 3 === 0){
+                fingers.style.backgroundColor = 'red';
+                fingers.style.color = 'white';
+                toes.style.backgroundColor = 'red';
+                toes.style.color = 'white';
+                displayNumber.innerHTML = newNumber.toString();
+            }
+            else if(newNumber % 5 === 0){
+                fingers.style.backgroundColor = 'transparent';
+                fingers.style.color = 'black';
+                toes.style.backgroundColor = 'red';
+                toes.style.color = 'white';
+                displayNumber.innerHTML = newNumber.toString();
+            }
+            else if(newNumber % 3 === 0){
+                fingers.style.backgroundColor = 'red';
+                fingers.style.color = 'white';
+                toes.style.backgroundColor = 'transparent';
+                toes.style.color = 'black';
+                displayNumber.innerHTML = newNumber.toString();
+            }
+            else{
+                fingers.style.backgroundColor = 'transparent';
+                fingers.style.color = 'black';
+                toes.style.backgroundColor = 'transparent';
+                toes.style.color = 'black';
+                displayNumber.innerHTML = newNumber.toString();
+            }
+        }
+        if(newNumber == maxNumber){
+            if(newNumber % 5 === 0 && newNumber % 3 === 0){
+                fingers.style.backgroundColor = 'red';
+                fingers.style.color = 'white';
+                toes.style.backgroundColor = 'red';
+                toes.style.color = 'white';
+                displayNumber.innerHTML = newNumber.toString();
+            }
+            else if(newNumber % 5 === 0){
+                fingers.style.backgroundColor = 'transparent';
+                fingers.style.color = 'black';
+                toes.style.backgroundColor = 'red';
+                toes.style.color = 'white';
+                displayNumber.innerHTML = newNumber.toString();
+            }
+            else if(newNumber % 3 === 0){
+                fingers.style.backgroundColor = 'red';
+                fingers.style.color = 'white';
+                toes.style.backgroundColor = 'transparent';
+                toes.style.color = 'black';
+                displayNumber.innerHTML = newNumber.toString();
+            }
+            else{
+                fingers.style.backgroundColor = 'transparent';
+                fingers.style.color = 'black';
+                toes.style.backgroundColor = 'transparent';
+                toes.style.color = 'black';
+                displayNumber.innerHTML = newNumber.toString();
+            }
+            clearInterval(intervalId);
+        }
+    },1000)
+}
